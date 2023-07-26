951e5d82f36db2c24acce8e2a48e9869c3ba940e
SynDevUI <133002989+SynDevUI@users.noreply.github.com>
2023-07-26 08:44:21 +0200

    Update useOnClickOutside.ts

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -1,28 +1,25 @@
-import { RefObject, useEffect, MouseEvent as ReactMouseEvent } from 'react';
+import * as React from 'react';
+import { useEffect, useRef } from 'react';
 
-type EventType = MouseEvent | TouchEvent;
+type OnClickOutsideCallback = () => void;
+const useOnClickOutside = (
+  ref: React.RefObject<HTMLElement>,
+  callback: OnClickOutsideCallback
+) => {
+  const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      callback?.();
+    }
+  };
 
-export const useOnClickOutside = (
-	refs: RefObject<HTMLElement>[],
-	callback: () => void,
-	condition: boolean = true,
-): void => {
-	useEffect(() => {
-		const handleClickOutside = (event: EventType): void => {
-			if (
-				condition &&
-				refs.every((ref) => ref.current && !ref.current.contains(event.target as Node))
-			) {
-				callback();
-			}
-		};
-
-		// Bind the listener to the document
-		const boundListener = handleClickOutside.bind(document);
-
-		document.addEventListener('mousedown', boundListener);
-		return () => {
-			document.removeEventListener('mousedown', boundListener);
-		};
-	}, [refs, callback, condition]);
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('touchstart', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
+    };
+  }, [ref, callback]);
 };
+
+export default useOnClickOutside;
